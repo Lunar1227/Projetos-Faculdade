4db042fd49f7a6e83d7118bfd39fe1e800fed9ce
Eric Araujo <eric.araujoxavier@hotmail.com>
2024-10-17 00:01:09 -0300

    Update validacaologin.js

--- validacaologin.js
+++ validacaologin.js
@@ -1,3 +1,26 @@
+function recuperardados(){
+
+    var campousuario = document.getElementById('usuario').value;
+
+    var camposenha = document.getElementById('senha').value;
+
+    var login = localStorage.getItem('usuario1');
+
+    var senha = localStorage.getItem('senha1');
+
+    
+    if (campousuario == login && camposenha === senha) {
+        alert('Login efetuado com sucesso!')
+        window.location.href = "../html/telasobrenos.html";
+
+    } else {
+        alert('Usuário ou senha incorreto')
+    } 
+    
+}
+
+
+//Verifica se os campos estão preenchidos ou não
 function validarlogin() {
 
     var usuario = document.getElementById('usuario').value;
@@ -6,14 +29,16 @@ function validarlogin() {
 
     if (usuario === "") {
         alert('Usuário não informado');
-        return false
+        return false;
     }
 
     if (senha === "") {
         alert('Senha não informada')
-        return false
+        return false;
     }
 
-    window.location.href = "telasobrenos.html";
+    recuperardados();
+    return false;
 
 } 
+
