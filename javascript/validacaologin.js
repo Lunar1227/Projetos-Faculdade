c319a381044c012ef4a1abe71dd64f7d56d3e664
Eric Araujo <eric.araujoxavier@hotmail.com>
2024-10-21 20:49:14 -0300

    Update validacaologin.js

--- javascript/validacaologin.js
+++ javascript/validacaologin.js
@@ -28,36 +28,58 @@
 // essa função Verifca se os campos estão preenchidos ou não
 
 
-function validarlogin() {
+// function validarlogin() {
 
-    var usuario = document.getElementById('usuario').value;
+//     var usuario = document.getElementById('usuario').value;
 
-    var senha = document.getElementById('senha').value;
+//     var senha = document.getElementById('senha').value;
 
-    if (usuario === "") {
-        alert('Usuário não informado');
-        return false;
-    }
+//     if (usuario === "") {
+//         alert('Usuário não informado');
+//         return false;
+//     }
 
-    if (senha === "") {
-        alert('Senha não informada')
-        return false;
-    }
+//     if (senha === "") {
+//         alert('Senha não informada')
+//         return false;
+//     }
 
-    senha_salva = JSON.parse(localStorage.getItem('chamandosenha'));
+//     senha_salva = JSON.parse(localStorage.getItem('chamandosenha'));
 
-    usuario_salvo = JSON.parse(localStorage.getItem('chamandousu'));
+//     usuario_salvo = JSON.parse(localStorage.getItem('chamandousu'));
 
-    if (usuario === usuario_salvo && senha === senha_salva) {
-        window.location.href = "../html/telasobrenos.html"
+//     if (usuario === usuario_salvo && senha === senha_salva) {
+//         window.location.href = "../html/telasobrenos.html"
 
 
+//     } else {
+//         alert ('Usuário ou senha incorreto!')
+//     }
+
+//     console.log('Os dados foram carregados!')
+//     return false;
+
+// } 
+
+
+
+function recuperardados() {
+    var campousuario = document.getElementById('usuario').value;
+    var camposenha = document.getElementById('senha').value;
+
+    // Recuperando os usuários do localStorage
+    var usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+    // Procurando o usuário que corresponde ao login e à senha
+    var usuarioEncontrado = usuarios.find(function(usuario) {
+        return usuario.login === campousuario && usuario.senha === camposenha;
+    });
+
+    if (usuarioEncontrado) {
+        alert('Login bem-sucedido!');
+        window.location.href = "../html/telasobrenos.html";
     } else {
-        alert ('Usuário ou senha incorreto!')
+        alert('Usuário ou senha incorretos.');
     }
-
-    console.log('Os dados foram carregados!')
-    return false;
-
-} 
+}
 
