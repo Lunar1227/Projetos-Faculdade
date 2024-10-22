40cde9a73c51066910f5a9f32fad97dc63716323
Eric Araujo <eric.araujoxavier@hotmail.com>
2024-10-22 00:02:42 -0300

    Update validacaologin.js

--- javascript/validacaologin.js
+++ javascript/validacaologin.js
@@ -1,80 +1,23 @@
 
-// function recuperardados(){
 
-
-//     var campousuario = document.getElementById('usuario').value;
-
-//     var camposenha = document.getElementById('senha').value;
-
-//     var usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-
-//     var usuarioEncontrado = usuarios.find(function SalvarDados() {
-//     return usuario.nome === campousuario && usuario.senha === camposenha;
-//     });
-
-//     if (usuarioEncontrado) {
-//     alert('Login bem-sucedido!');
-//     window.location.href = "../html/telasobrenos.html";
-//     } else {
-//     alert('Usuário ou senha incorretos.');
-    
-//     }
-// }
-
-
-
-// }
-
-// essa função Verifca se os campos estão preenchidos ou não
-
-
-// function validarlogin() {
-
-//     var usuario = document.getElementById('usuario').value;
-
-//     var senha = document.getElementById('senha').value;
-
-//     if (usuario === "") {
-//         alert('Usuário não informado');
-//         return false;
-//     }
-
-//     if (senha === "") {
-//         alert('Senha não informada')
-//         return false;
-//     }
-
-//     senha_salva = JSON.parse(localStorage.getItem('chamandosenha'));
-
-//     usuario_salvo = JSON.parse(localStorage.getItem('chamandousu'));
-
-//     if (usuario === usuario_salvo && senha === senha_salva) {
-//         window.location.href = "../html/telasobrenos.html"
-
-
-//     } else {
-//         alert ('Usuário ou senha incorreto!')
-//     }
-
-//     console.log('Os dados foram carregados!')
-//     return false;
-
-// } 
-
-
-
-function recuperardados() {
+//essa função pega os dados do input login e senha da tela de login;
+function recuperarDados() {
     var campousuario = document.getElementById('usuario').value;
     var camposenha = document.getElementById('senha').value;
 
-    // Recuperando os usuários do localStorage
+    //criei uma variável chamado usuarios, que está recebendo o valor do 
+    //LocalStorage.setItem, responsável por pegar os dados na página "testesalvardados"
     var usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
-    // Procurando o usuário que corresponde ao login e à senha
+    // Procurando o usuário que corresponde ao login e à senha. Para isso, criei uma variavel,
+    // daí coloquei o nome da variável que eu coloquei acima e .find para encontrar o objeto e as propriedades usuario e senha.
     var usuarioEncontrado = usuarios.find(function(usuario) {
         return usuario.login === campousuario && usuario.senha === camposenha;
     });
 
+
+    //Aqui é um if básico, se os dados da variável acima forem encontrados, aparece o alerta de login bem sucedido
+    // e direciona para a tela sobre nós, caso não vai alertar que o usuário e a senha estão incorretos.
     if (usuarioEncontrado) {
         alert('Login bem-sucedido!');
         window.location.href = "../html/telasobrenos.html";
